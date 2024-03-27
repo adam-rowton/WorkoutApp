57f593f1f95be3bee8914ef7fa468849dead1f46
Adam Rowton <adamlrowton@gmail.com>
2024-03-26 23:38:58 -0600

    Initial Commit

---
+++ Countdown/CountdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountdownApp.swift
+//  Countdown
+//
+//  Created by Adam Rowton on 3/26/24.
+//
+
+import SwiftUI
+
+@main
+struct CountdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
