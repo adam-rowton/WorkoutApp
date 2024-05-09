bb811365a05f570d18800db1c37d9d6aafa7812e
Adam Rowton <adamlrowton@gmail.com>
2024-05-09 08:56:21 -0600

    added exercise selection to workout builder

---
+++ WorkoutApp/Sheets/ExerciseSelectionSheet.swift
@@ -0,0 +1,18 @@
+//
+//  ExerciseSelectionSheet.swift
+//  WorkoutApp
+//
+//  Created by Adam Rowton on 5/8/24.
+//
+
+import SwiftUI
+
+struct ExerciseSelectionSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExerciseSelectionSheet()
+}
---
+++ WorkoutApp/Sheets/listSelectionTest.swift
@@ -0,0 +1,80 @@
+//
+//  WorkoutEditor.swift
+//  WorkoutApp
+//
+//  Created by Adam Rowton on 4/10/24.
+//
+
+import SwiftUI
+import SwiftData
+
+struct WorkoutEditortest: View {
+    @Environment(\.dismiss) var dismiss
+    
+    @Environment(\.modelContext) private var context
+    @State var selectedExercises: [Exercise] = []
+    
+    @State private var workoutName: String = ""
+    @State var isShowingSheet = false
+    
+    @Query var exercises: [Exercise]
+    
+    var body: some View {
+        VStack {
+            VStack {
+                HStack {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Text("Cancel")
+                    }
+                    
+                    Spacer()
+                    
+                    Button {
+                        // context.insert(exercise)
+                        try! context.save()
+                        dismiss()
+                        //save
+                    } label: {
+                        Text("Save")
+                    }
+                }
+                TextField("Exercise Name", text: $workoutName)
+            }.padding(20)
+            VStack {
+                if selectedExercises.isEmpty {
+                    VStack {
+                        Text("No Exercises Selected")
+                        Button {
+                            isShowingSheet.toggle()
+                        } label: {
+                            Text("Add An Exercise")
+                        }
+                    }
+                  
+                } else {
+                    List{
+                        ForEach(selectedExercises){ exercise in
+                            Text(exercise.name)
+                        }
+                    }
+                    .sheet(isPresented: $isShowingSheet) {
+                        ExerciseSelectionSheet().presentationDetents([.medium]) }
+                    
+                    Button {
+                        isShowingSheet.toggle()
+                    } label: {
+                        Text("Add Another Exercise")
+                    }
+                }
+                
+            }
+        }
+    }
+}
+
+
+#Preview {
+    WorkoutEditortest()
+}
