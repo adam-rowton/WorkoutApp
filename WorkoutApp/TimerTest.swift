b9f31062106046bbc2afe84c425e2553f61a09b3
Adam Rowton <adamlrowton@gmail.com>
2024-05-22 15:17:31 -0600

    Added functionality to save workouts

---
+++ WorkoutApp/TimerTest.swift
@@ -0,0 +1,18 @@
+//
+//  TimerTest.swift
+//  WorkoutApp
+//
+//  Created by Adam Rowton on 5/22/24.
+//
+
+import SwiftUI
+
+struct TimerTest: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TimerTest()
+}
